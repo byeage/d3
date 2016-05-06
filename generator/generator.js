ffd9cc79a2ce118705a8d0431e58fe2740f1a857
byeage <lihuazhidao@gmail.com>
2016-05-06 16:32:36 +0800

    弧线生成器

--- generator/generator.js
+++ generator/generator.js
@@ -10,26 +10,59 @@ var svg=d3.select("body")
 			.attr("height", height);
 
 
-/*区域生成器*/
-var dataset=[80,120,130,70,60,90];
+/*弧生成器*/
 
-var areaPath=d3.svg.area()
-				.interpolate("basis")
-				.interpolate("cardinal")
-				.interpolate("step")
-				.x(function(d,i){
-					return 50+i*80;
-				})
-				.y0(function(d,i){
-					return height/2;
-				})
-				.y1(function(d,i){
-					return height/2-d;
-				});
+/**
+ * 起始角度 startAngle
+ * 终止角度 endAngle
+ * 内半径 innerRadius
+ * 外半径 outerRadius
+ */
 
-	
-svg.append("path")
-	.attr("d",areaPath(dataset))
-	.attr("stroke","#000")
-	.attr("stroke-width","3px")
-	.attr("fill","yellow");
+
+var dataset=[
+			{
+				startAngle:0,
+				endAngle:Math.PI*0.75
+			},
+			{
+				startAngle:Math.PI*0.75,
+				endAngle:Math.PI*1
+			}
+			];
+
+
+var color=d3.scale.category10();
+var arcPath=d3.svg.arc()
+				.innerRadius(0)
+				.outerRadius(100);
+	svg.selectAll("path")
+		.data(dataset)
+		.enter()
+		.append("path")
+		.attr("d",function(d,i){
+			return arcPath(d);
+		})
+		.attr("transform","translate(250,250)")
+		.attr("stroke-width","3px")
+		.attr("stroke","#000")
+		.attr("fill",function(d,i){
+			return color(i);
+		});	
+
+/*给每段弧线添加文字*/
+
+svg.selectAll("text")
+	.data(dataset)
+	.enter()
+	.append("text")
+	.attr("transform",function(d,i){
+		return "translate(250,250)"+
+			"translate("+arcPath.centroid(d)+")";
+	})
+	.attr("text-anchor","middle")
+	.attr("fill","#fff")
+	.attr("font-size","18px")
+	.text(function(d,i){
+		return Math.floor((d.endAngle-d.startAngle)*180/Math.PI)+"'";
+	});
